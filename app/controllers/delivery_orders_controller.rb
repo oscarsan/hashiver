02330a57b13333300568cd0a73de82703ffddb42
oscar sanhueza <oscar@hashdot.fi>
2017-02-06 23:43:58 +0200

    delivery order model and controllers
    adds show, index and edit to controller
    adds model function to update delivery order status
    
    Signed-off-by: oscar sanhueza <oscar@hashdot.fi>

--- app/controllers/delivery_orders_controller.rb
+++ app/controllers/delivery_orders_controller.rb
@@ -3,4 +3,28 @@ class DeliveryOrdersController < ApplicationController
   def index
      @delivery_orders = DeliveryOrder.all
   end
+
+  def show
+    @delivery_order = DeliveryOrder.find(params[:id])
+  end
+
+  def edit
+    @delivery_order = DeliveryOrder.find(params[:id])
+  end
+
+  def update
+    @delivery_order = DeliveryOrder.find(params[:id])
+
+    if @delivery_order.process_order
+      redirect_to @delivery_order
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def delivery_order_params
+      params.require(:delivery_order).permit(:state)
+    end
+
 end
--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -1,2 +1,10 @@
+require 'odoo/import'
+
 class DeliveryOrder < ApplicationRecord
+    include Odoo::Import
+    def process_order
+      process_delivery_order(self['odoo_id'])
+      return true
+    end
+
 end
